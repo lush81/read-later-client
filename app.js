d8ccba997fbe2ded270ab09be71bce8343beafd3
lush81 <lush@i.ua>
2015-04-02 17:31:03 +0000

    client_serv

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var path = require('path'); // модуль для парсинга пути
+var bodyParser = require('body-parser');
 var app = express();
 
-app.use(express.logger('dev')); // выводим все запросы со статусами в консоль
-app.use(express.bodyParser()); // стандартный модуль, для парсинга JSON в запросах
+//app.use(express.logger('dev')); // выводим все запросы со статусами в консоль
+app.use(bodyParser()); // стандартный модуль, для парсинга JSON в запросах
 
 app.use(express.static(path.join(__dirname, "public"))); // запуск статического файлового сервера, который смотрит на папку public/ (в нашем случае отдает index.html)
 
